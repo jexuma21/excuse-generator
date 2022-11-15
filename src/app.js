8099ad56c0f7caa5c8376133101b3d4996720816
jexuma21 <jerryexuma21@gmail.com>
2022-11-15 00:45:15 +0000

    project finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let sentence =
+    who[rand(who.length)] +
+    " " +
+    action[rand(action.length)] +
+    " " +
+    what[rand(what.length)] +
+    " " +
+    when[rand(when.length)];
+  let excuseSpan = document.querySelector("#excuse");
+  excuseSpan.innerHTML = sentence;
 };
+function rand(length) {
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
